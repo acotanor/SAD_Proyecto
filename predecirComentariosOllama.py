274bf997d79e851d4e4b2a297231168133071018
plopez053 <123484569+plopez053@users.noreply.github.com>
2025-05-25 17:30:20 +0200

    Add files via upload

--- predecirComentariosOllama.py
+++ predecirComentariosOllama.py
@@ -38,15 +38,14 @@ Rating:"""
     return consulta_ollama(prompt)
 
 def prediccion_2shots(comment):
-    prompt = f"""Give a rating from 1 to 9 for the following hotel review. Only answer with the number.
+    prompt = f"""Give a rating from 1 to 10 for the following hotel review. Only answer with the number.
 Example 1:
-Review: "Lot of Traffic noise due to old windows toward the street. And the  guys where delayed  more than 1 hour we where waiting 
-in front of the building and couldtn get in contact with the guys. Finally we came inside then the cleaning team has not been there so it was dirty used beeds beer cans used towels bed linen etc. so i had to wait another 2 hours before i could use the apartment . that was not nice after  a long day of traveling"
-Rating: 2
+Review: "We had a spledid time in the old centre of Porto. The appartment is very well situated next to the old Ribeira square. It's perfect to have such an appartment to your disposal, you feel home, and have a place to relax between the exploration of this very nice city. We thank Ana & Gonçalo, and we hope the appartment is free when we go back next year. Porto is charming original"
+Rating: 9
 
-Example 2:
-Review: "It's a nice flat. The location is good even though you have to walk a bit to the metro and quite a bit to the historic center. I liked that there was a restaurant in the same building."
-Rating: 5
+Example 5:
+Review :" It was our first time using Airbnb' and it was really good. Thanks for everything!"
+Rating: 8
 
 Now, rate this review:
 Review: "{comment}"
@@ -54,26 +53,26 @@ Rating:"""
     return consulta_ollama(prompt)
 
 def prediccion_fewShots(comment):
-    prompt = f"""Give a rating from 1 to 9 for the following hotel review. Only answer with the number.
+    prompt = f"""Give a rating from 1 to 10 for the following hotel review. Only answer with the number.
 Example 1:
 Review: "We had a spledid time in the old centre of Porto. The appartment is very well situated next to the old Ribeira square. It's perfect to have such an appartment to your disposal, you feel home, and have a place to relax between the exploration of this very nice city. We thank Ana & Gonçalo, and we hope the appartment is free when we go back next year. Porto is charming original"
-Rating: 9
+Rating: 10
 
 Example 2:
 Review: "It's a nice flat. The location is good even though you have to walk a bit to the metro and quite a bit to the historic center. I liked that there was a restaurant in the same building."
 Rating: 6
 
 Example 3:
-Review: "Lot of Traffic noise due to old windows toward the street. And the  guys where delayed  more than 1 hour we where waiting"
-Rating: 2
+Review: "Great stay, would come back!"
+Rating: 8
 
 Example 4:
 Review:  "The apartment was as we expected and I recommend it to other travelers."
 Rating: 5
 
 Example 5:
-Review :" It was our first time using Airbnb' and it was really good. Thanks for everything!"
-Rating: 8
+Review :" "The apartment was filthy, smelled terrible, and nothing worked. The bed was broken, there were bugs everywhere, and the host never responded to our messages. We left after the first night because it was impossible to stay there. Absolutely the worst experience I've ever had.""
+Rating: 1
 
 Now, rate this review:
 Review: "{comment}"
